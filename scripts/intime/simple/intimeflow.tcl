e5d41a6a0866fca16e57b236c46ed47e7dad6d1f
Harn Hua Ng <harnhua@users.noreply.github.com>
2019-08-13 23:25:32 +0800

    Update intimeflow.tcl
    Added comments

--- scripts/intime/simple/intimeflow.tcl
+++ scripts/intime/simple/intimeflow.tcl
@@ -1,4 +1,5 @@
-# Sample script for InTime Optimization. Works Windows & Linux
+# Sample script for InTime Optimization. Works in Windows & Linux environments
+# Adjust the run_target, runs_per_round and round variables according to your recipe
 flow reset
 flow set run_target local
 flow set runs_per_round 2
