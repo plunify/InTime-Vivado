c7ac1f4a75950187305c544f162ee4713fa07bdb
Harn Hua Ng <harnhua@users.noreply.github.com>
2021-07-12 10:31:35 +0800

    Update intimeflow.tcl
    Updated script name in the header

--- scripts/intime/simple/intimeflow.tcl
+++ scripts/intime/simple/intimeflow.tcl
@@ -1,20 +1,20 @@
 #----------------------------------------------------------------------
-# Script      : autorun_multi_recipes.tcl
-# Description : auto run multiple InTime Recipes in a user-defined order
+# Script      : intimeflow.tcl
+# Description : Example on how to run the Hot Start recipe via Tcl
 # Author      : Plunify
 # Version     : 1.1
 #               
 # Return : None
 #
 # Usage :
-#    1. Via Intime Tcl console
+#    1. Via the InTime Tcl console
 #       % source <script.tcl>
 #
-#    2. Via Command-line
+#    2. Via the Command-line
 #       % intime.sh -mode batch -platform minimal -s <script.tcl>
 #----------------------------------------------------------------------
 
-# Sample script for InTime Optimization. Works in Windows & Linux environments
+# Sample script for InTime Optimization
 # Adjust the run_target, runs_per_round and round variables according to your recipe
 flow reset
 flow set run_target local
